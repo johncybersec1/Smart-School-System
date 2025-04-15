45237553f836aa1e7ead353f1c91463faa46efa0
johncybersec1 <mwangijohn294@gmail.com>
2025-04-15 10:20:40 +0200

    added templates

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 
